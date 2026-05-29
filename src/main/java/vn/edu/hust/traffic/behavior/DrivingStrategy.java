d63c3963a51310bf35d7d67f1f8239611a8f60e2
dempseyindahouse <trunghieu17112004@gmail.com>
2026-05-29 11:17:00 +0700

    Refactor: Delete unused/dead code (duplicate view.VehicleRenderer and empty behavior strategy placeholder files)

--- src/main/java/vn/edu/hust/traffic/behavior/DrivingStrategy.java
+++
@@ -1,6 +0,0 @@
-package vn.edu.hust.traffic.behavior;
-
-// Chiến lược lái xe 
-public interface DrivingStrategy {
-    void drive();
-}
--- src/main/java/vn/edu/hust/traffic/behavior/NormalDriver.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.hust.traffic.behavior;
-
-// Người lái xe bình thường 
-public class NormalDriver implements DrivingStrategy {
-    @Override
-    public void drive() {
-        // TODO: Logic lái xe bình thường
-    }
-}
